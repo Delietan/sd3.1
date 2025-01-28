b2f27c9354188b3042d637c6f7559d5e3816e03f
Delietan <martinusnord@gmail.com>
2025-01-28 02:51:01 +0100

    big

--- models/index.js
+++ models/index.js
@@ -1,33 +1,42 @@
-const Sequelize = require('sequelize')
-const fs = require("fs")
-const path = require("path")
+const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
 const basename = path.basename(__filename);
-require('dotenv').config()
+require('dotenv').config();
+
 const connection = {
   database: process.env.DATABASE_NAME,
   username: process.env.ADMIN_USERNAME,
   password: process.env.ADMIN_PASSWORD,
-  port: process.env.PORT,
   host: process.env.HOST,
-  dialect: process.env.DIALECT,
-  dialectmodel: process.env.DIALECTMODEL,
+  port: process.env.PORT, // Ensure this is the DB port (not your app port)
+  dialect: process.env.DIALECT, // Example: 'postgres' or 'mysql'
+  dialectModule: process.env.DIALECTMODEL ? require(process.env.DIALECTMODEL) : undefined, // Require the module directly
 };
-const sequelize = new Sequelize(connection);
-const db = {}
-db.sequelize = sequelize
+
+const sequelize = new Sequelize(connection.database, connection.username, connection.password, {
+  host: connection.host,
+  port: connection.port,
+  dialect: connection.dialect,
+  dialectModule: connection.dialectModule, // Pass the required module
+});
+
+const db = {};
+db.sequelize = sequelize;
+
 fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) &&   
-      (file.slice(-3) === '.js');
-    })
-  .forEach(file => {    
-    const model = require(path.join(__dirname, file))(sequelize,   
-      Sequelize);
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
     db[model.name] = model;
   });
+
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-module.exports = db
+
+module.exports = db;
